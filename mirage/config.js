2a91e3db99e70ddaa3af30a9f3fa61ae926ff18e
Stephane Bruckert <contact@stephanebruckert.com>
2017-03-13 21:02:47 +0000

    Fix tests and styling

--- mirage/config.js
+++ mirage/config.js
@@ -5,18 +5,18 @@ export default function() {
     var number = function(n, value) {
       value += 1;
       var fizzbuzz = '';
-      fizzbuzz += (value % 3 == 0 ? 'Fizz' : '');
-      fizzbuzz += (value % 5 == 0 ? 'Buzz' : '');
+      fizzbuzz += (value % 3 === 0 ? 'Fizz' : '');
+      fizzbuzz += (value % 5 === 0 ? 'Buzz' : '');
       return {
         type: 'numbers',
         id: value,
         attributes: {
           number: fizzbuzz || value
         }
-      }
-    }
+      };
+    };
     return {
       data: Array(100).fill(1).map(number)
-    }
+    };
   });
 }
--- tests/acceptance/list-numbers-test.js
+++ tests/acceptance/list-numbers-test.js
@@ -3,9 +3,9 @@ import moduleForAcceptance from 'fizz-buzz-spa/tests/helpers/module-for-acceptan
 
 moduleForAcceptance('Acceptance | list numbers');
 
-test('should list first 5 numbers', function (assert) {
+test('should list first 100 numbers', function (assert) {
   visit('/');
   andThen(function() {
-    assert.equal(find('li').length, 5, 'should see 5 listings');
+    assert.equal(find('li').length, 100, 'should see 100 listings');
   });
 });
