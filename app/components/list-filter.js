dbf067ed40784ed7143529ae519d1d8dca276794
Stephane Bruckert <contact@stephanebruckert.com>
2017-03-14 18:44:50 +0000

    Improve filter component with previous and next actions

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -12,8 +12,21 @@ export default Ember.Component.extend({
   actions: {
     handleFilterEntry() {
       let filterInputValue = this.get('value');
+      this.set('value', filterInputValue);
       let filterAction = this.get('filter');
       filterAction(filterInputValue).then((filterResults) => this.set('results', filterResults));
+    },
+
+    next() {
+      this.set('value', (parseInt(this.get('value')) || 1) + 100);
+      let filterAction = this.get('filter');
+      filterAction(this.get('value')).then((filterResults) => this.set('results', filterResults));
+    },
+
+    previous() {
+      this.set('value', this.get('value') - 100);
+      let filterAction = this.get('filter');
+      filterAction(this.get('value')).then((filterResults) => this.set('results', filterResults));
     }
   }
 });
